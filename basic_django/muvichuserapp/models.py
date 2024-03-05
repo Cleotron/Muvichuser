171b4cfac056787dcb68eb6946a522a466015c1d
Cleotron <Cleotron@users.noreply.github.com>
2024-03-05 20:56:34 +0100

    make priority an enum

--- basic_django/muvichuserapp/models.py
+++ basic_django/muvichuserapp/models.py
@@ -6,7 +6,18 @@ from django.utils import timezone
 class Movie(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    priority = models.TextField()
+
+    class Priority(models.TextChoices):
+        HIGH = "1"
+        MEDIUM = "2"
+        LOW = "3"
+
+    priority = models.CharField(
+        max_length=1,
+        choices=Priority.choices,
+        default=Priority.HIGH,
+    )
+
 
     def publish(self):
         self.published_date = timezone.now()
